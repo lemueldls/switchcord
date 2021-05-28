1d49afc3113ae37116f451f503305c187f1016d0
Lemuel DLS <lemueld73@gmail.com>
2021-05-28 00:42:26 -0400

    🗑 Cleanup

--- main/index.ts
+++ main/index.ts
@@ -1,13 +1,7 @@
 import { app, BrowserWindow } from "electron";
 
 import "./ipc";
-
-import { autoUpdater } from "electron-updater";
-import log from "consola";
-
-autoUpdater.logger = log;
-// autoUpdater.logger.transports.file.level = "info";
-log.info("App starting...");
+import updater from "./updater";
 
 async function loadDevtools({ webContents }: BrowserWindow) {
   webContents.openDevTools();
@@ -24,32 +18,30 @@ async function loadDevtools({ webContents }: BrowserWindow) {
   // );
 }
 
-let win: BrowserWindow;
-
 function createWindow() {
-  const isProduction = process.env.NODE_ENV === "production";
+  const icon = `resources/icon.${process.platform === "win32" ? "ico" : "png"}`;
 
-  win = new BrowserWindow({
+  const win = new BrowserWindow({
     width: 1000,
     height: 600,
     webPreferences: {
-      // webSecurity: isProduction, // Disable on dev to allow loading local resources
       nodeIntegration: true, // Allow loading modules via the `require()` function
       contextIsolation: false, // https://github.com/electron/electron/issues/18037#issuecomment-806320028
     },
-    icon: "resources/icon.ico",
+    icon,
   });
 
   win.setMenu(null);
 
-  if (isProduction) win.loadFile("renderer/dist/index.html");
+  if (process.env.NODE_ENV === "production")
+    win.loadFile("renderer/dist/index.html");
   else {
     win.loadURL("http://localhost:3000");
 
     loadDevtools(win);
   }
 
-  autoUpdater.checkForUpdatesAndNotify();
+  updater.checkForUpdatesAndNotify();
 }
 
 app.whenReady().then(createWindow);
@@ -60,31 +52,4 @@ app.on("window-all-closed", () => {
 
 app.on("activate", () => {
   if (BrowserWindow.getAllWindows().length === 0) createWindow();
-});
-
-function sendStatusToWindow(text: string) {
-  log.info(text);
-  win.webContents.send("message", text);
-}
-
-autoUpdater.on("checking-for-update", () => {
-  sendStatusToWindow("Checking for update...");
-});
-autoUpdater.on("update-available", info => {
-  sendStatusToWindow("Update available.");
-});
-autoUpdater.on("update-not-available", info => {
-  sendStatusToWindow("Update not available.");
-});
-autoUpdater.on("error", error => {
-  sendStatusToWindow(`Error in auto-updater. ${error}`);
-});
-autoUpdater.on("download-progress", progressObject => {
-  let message = `Download speed: ${progressObject.bytesPerSecond}`;
-  message = `${message} - Downloaded ${progressObject.percent}%`;
-  message = `${message} (${progressObject.transferred}/${progressObject.total})`;
-  sendStatusToWindow(message);
-});
-autoUpdater.on("update-downloaded", info => {
-  sendStatusToWindow("Update downloaded");
 });
